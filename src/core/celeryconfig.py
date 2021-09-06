9f04bfeae92b774b24baa5bcb6017e0121eaf117
Zephyr <zephyr2201@gmail.com>
2021-09-06 22:58:04 +0600

    Final commit

--- src/core/celeryconfig.py
+++
@@ -1,5 +0,0 @@
-timezone = "Asia/Almaty"
-task_track_started = True
-task_time_limit = 180 * 60
-broker_url = 'redis://:pd1ef0181133a436b0d7097d867d8d3127af6cb9f192d5a4f41ac919568fb06ca@ec2-44-197-56-240.compute-1.amazonaws.com:13030'
-send_events = True
